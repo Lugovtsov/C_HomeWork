19b1a07fe34db2189031a588193476ffbe5f6e88
lugovtsov <lugovtsov_ak@orbitasporta.com>
2023-09-16 15:31:36 +0700

    added Task41, Task43

---
+++ Task41/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+*/
+
+int[] GetUserArray()
+{
+    System.Console.WriteLine("Задайте размер массива");
+
+    int size = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Заполни массив");
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine("]");
+}
+
+int[] userarray = GetUserArray();
+PrintArray(userarray);
+
+int SerchPositivNumbers(int[] array)
+{
+    int result = 0;  
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            result = result + 1;
+        }
+    }
+    return result;
+}
+System.Console.WriteLine($"пользователь ввел {SerchPositivNumbers(userarray)} чисел больше нуля");
---
+++ Task43/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+43 Напишите программу, которая найдёт точку пересечения двух прямых,
+ заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+*/
+/*
+|y=k1*x+b1   |y-y=k1x+b1-k2x-b2    |0=k1x-k2x-b2+b1    |k2x-k1x=b1-b2    |x=(b1-b2)/(k2-k1)
+|         => |                  => |                => |              => |
+|y=k2*x+b2   |y=k2x+b2             |y=k2x+b2           |y=k2x+b2         |y=k2x+b2
+*/
+System.Console.WriteLine("Введите значения b1, k1, b2, k2");
+double b1 = Convert.ToInt32(System.Console.ReadLine());
+double k1 = Convert.ToInt32(System.Console.ReadLine());
+double b2 = Convert.ToInt32(System.Console.ReadLine());
+double k2 = Convert.ToInt32(System.Console.ReadLine());
+
+if (k1 == k2)
+{
+    System.Console.WriteLine("Прямые параллельны");
+}
+else
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = k2 * x + b2;
+
+    System.Console.WriteLine($"Точка пересечения прямых  {Math.Round(x, 2)}:{Math.Round(y, 2)}");
+}
