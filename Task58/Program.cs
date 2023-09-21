9ce220609a8fead8f12b073c9c6e9306d43767b7
lugovtsov <lugovtsov_ak@orbitasporta.com>
2023-09-21 23:33:07 +0700

    added Task54, Task56, Task58, Task60, Task62

---
+++ Task58/Program.cs
@@ -0,0 +1,60 @@
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+*/
+
+﻿/*
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+*/
+
+
+int[,] MultiplicationMatrix(int[,] MatrixOne, int[,] MatrixTwo)
+{
+    int[,] result = new int[MatrixOne.GetLength(0), MatrixTwo.GetLength(1)];
+    for (var i = 0; i < MatrixOne.GetLength(0); i++)
+    {
+        for (var j = 0; j < MatrixTwo.GetLength(0); j++)
+        {
+            for (var x = 0; x < MatrixTwo.GetLength(1); x++)
+            {
+                result[i, x] += MatrixOne[i, j] * MatrixTwo[j, x];
+            }
+        }
+    }
+    return result;
+}
+int[,] Array(int row, int col)
+{
+    int[,] array = new int[row,col];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,5);
+        }
+    }
+    return array;
+
+}
+Console.Write("Введите размер матрицы: ");
+int sizeMatrix = Convert.ToInt32(Console.ReadLine());
+var MatrixOne = new int[sizeMatrix, sizeMatrix];
+var MatrixTwo = new int[sizeMatrix, sizeMatrix];
+int[,] MatrixArrOne = Array(sizeMatrix,sizeMatrix);
+int[,] MatrixArrTwo = Array(sizeMatrix,sizeMatrix);
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i,j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+PrintArray(MatrixArrOne);
+System.Console.WriteLine();
+PrintArray(MatrixArrTwo);
+System.Console.WriteLine();
+int[,] result = MultiplicationMatrix(MatrixArrOne, MatrixArrTwo);
+PrintArray(result);
---
+++ Task62/Program.cs
@@ -0,0 +1,90 @@
+﻿/* Задача 62: Заполните спирально массив 4 на 4. или (n на n) */
+
+
+Console.Clear();
+
+System.Console.Write("Высота массива:");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Ширина массива:");
+int cols = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+int[,] array = Spiral(rows, cols);
+int[,] Spiral(int rows, int cols)
+{
+    int[,] array = new int[rows, cols];            
+    int end = rows * cols;                          
+    int right = cols - 1;                          
+    int bottom = rows - 1;                        
+    int left = 0;                               
+    int top = 1;                                    
+    int direction = 1;                              
+    int y = 0;                                     
+    int x = 0;
+    int pause = 500;                                
+
+    for (int i = 1; i <= end; i++)
+    {
+        array[y, x] = i;                            
+        PrintCurrent(array, y, x);                  
+        Thread.Sleep(pause);                        
+
+        if (direction == 1 && x < right) x++;       
+        else if (direction == 1 && x == right)      
+        {
+            direction = 2;                          
+            right--;                                
+        }
+        if (direction == 2 && y < bottom) y++;      
+        else if (direction == 2 && y == bottom)     
+        {
+            direction = 3;                          
+            bottom--;                              
+        }
+        if (direction == 3 && x > left) x--;        
+        else if (direction == 3 && x == left)       
+        {
+            direction = 4;                          
+            left++;                                 
+        }
+        if (direction == 4 && y > top) y--;         
+        else if (direction == 4 && y == top)        
+        {
+            direction = 1;                         
+            top++;                                  
+            x++;                                   
+        }
+    }
+    return array;
+}
+void PrintCurrent(int[,] array, int y, int x)                  
+{
+    Console.SetCursorPosition(0, 0);                            
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (j == x && i == y)                               
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                System.Console.Write(array[i, j] + "\t");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (array[i, j] != 0)                          
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.Write(array[i, j] + "\t");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else                                                
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+                System.Console.Write(array[i, j] + "\t");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+        }
+        System.Console.WriteLine();
+    }
+}
