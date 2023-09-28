9e402ffacf7a8e91e2ce9649d612fb577ea161d3
lugovtsov <lugovtsov_ak@orbitasporta.com>
2023-09-28 21:53:52 +0700

    added Task64, Task66, Task68

---
+++ Task64/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+64.Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+void NumberN(int num)
+{
+    if(num < 0)
+    num = num * -1; 
+    if( num != 0)
+    {
+        System.Console.Write(num + " ");
+        NumberN(num - 1);
+    }
+}
+System.Console.WriteLine("Введите число");
+int usernum = Convert.ToInt32(Console.ReadLine());
+NumberN(usernum);
+*/
---
+++ Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+66.Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int SumNumbMN(int M, int N)
+{
+
+    if (M < N)
+    {
+        return M + SumNumbMN(M + 1, N);
+    }
+    return M;
+}
+
+System.Console.WriteLine("Введите два числа ");
+int M = int.Parse(Console.ReadLine());
+int N = int.Parse(Console.ReadLine());
+System.Console.WriteLine(SumNumbMN(M, N));
